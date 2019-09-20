975d52feaf0e1e940c6d2d91d52865c88fa61098
Eric Mesa <ericsbinaryworld@gmail.com>
2019-09-20 16:18:24 -0400

    enlarged the window

--- amortization-gui.py
+++ amortization-gui.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QDialog, QApplication
 import sys
+import csv
 from gui import *
 import amortization
 
@@ -9,12 +10,18 @@ class AmortGUI(QDialog):
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.ui.pushButtonCalculate.clicked.connect(self.calculate)
+        self.model = QtGui.QStandardItemModel(self)
+        self.ui.tableViewCSV.setModel(self.model)
     def calculate(self):
         self.principal = int(self.ui.lineEditPrinciple.text())
         self.interest = float(self.ui.lineEditInterest.text())/12
         self.number_of_payments = int(self.ui.lineEditMonth.text())
         self.monthly_payment = (self.principal*self.interest)/(1-pow((1+self.interest), -self.number_of_payments))
         amortization.output(self.principal, self.interest, self.number_of_payments, self.monthly_payment, "csv")
+        with open("amort.csv","r") as csvinput:
+            for row in csv.reader(csvinput):
+                items = [QtGui.QStandardItem(field) for field in row]
+                self.model.appendRow(items)
         
 
 
